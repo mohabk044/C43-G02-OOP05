6fa78931305ce75e95c0d3ed676bb2570ce641e0
Mohab Khaled <mohabk044@gmail.com>
2025-01-04 22:10:02 +0200

    Second Project

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Assignment.First_Project;
+using Assignment.Second_Project;
+
 namespace Assignment
 {
     internal class Program
@@ -30,6 +32,43 @@ namespace Assignment
             //Console.WriteLine($"Divided: {Maths.Divided(8, 2)}");
             //Console.WriteLine($"Divided: {Maths.Divided(8, 33)}");
             //Console.WriteLine(Maths.Divided(8, 0)); 
+            #endregion
+
+            #region Second Project
+            Duration D1 = new Duration(1, 10, 15);
+            Console.WriteLine(D1.ToString());
+            Duration D2 = new Duration(3600);
+            Console.WriteLine(D2.ToString());
+            Duration D3 = new Duration(7800);
+            //Console.WriteLine(D3.ToString()); 
+            Duration D4 = new Duration(666);
+            //Console.WriteLine(D4.ToString());
+
+            //D3 = D1 + D2;
+            //Console.WriteLine(D3);
+            //D3 = D1 + 7800;
+            //Console.WriteLine(D3);
+            //D3 = 666 + D3;
+            //Console.WriteLine(D3);
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1);
+            //D3 = ++D1;  // In Minutes
+            //Console.WriteLine(D3);
+            //D3 = --D2;  // In Minutes
+            //Console.WriteLine(D3);
+
+            //if (D1 > D2)
+            //    Console.WriteLine($"Duration1 is bigger!");
+            //else
+            //    Console.WriteLine($"Duration2 is bigger!");
+
+            //if (D1 <= D2)
+            //    Console.WriteLine($"Duration2 is greater than or equal Duration1!");
+            //else
+            //    Console.WriteLine($"Duration1 is bigger!");
+
+            DateTime obj = (DateTime)D1;  // Valid
+            Console.WriteLine(obj);
             #endregion
         }
     }
