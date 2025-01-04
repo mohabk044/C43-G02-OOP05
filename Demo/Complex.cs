44cf2b204388d13267cd3b9f4b363c69ed91f4fa
Mohab Khaled <mohabk044@gmail.com>
2025-01-04 04:36:28 +0200

    Operators overloading - Relational

---
+++ Demo/Complex.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+        #region Opeartor Overloading
+        public static Complex operator +(Complex Left, Complex Right)
+        {
+            return new Complex()
+            {
+                Real = Left.Real + Right.Real,
+                Imag = Left.Imag + Right.Imag
+            };
+        }
+        #endregion
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+
+        public static Complex operator ++(Complex C)
+        {
+            return new Complex()
+            {
+                Real = (C?.Real ?? 0) + 1,
+                Imag = (C?.Imag ?? 0)
+            };
+        }
+        public static bool operator >(Complex left, Complex right)
+        {
+
+            if (left.Real == right.Real)
+                return left.Imag > right.Imag;
+            else
+                return left.Real > right.Real;
+
+        }
+
+        public static bool operator <(Complex left, Complex right)
+        {
+            if (left.Real == right.Real)
+                return left.Imag < right.Imag;
+            else
+                return left.Real < right.Real;
+
+        }
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Operators overloading - Relational
+            Complex C1 = new Complex() { Real = 1, Imag = 2 };
+            Complex C2 = new Complex() { Real = 2 , Imag = 4 };
+
+            if (C1 > C2)
+                Console.WriteLine("C1 > C2");
+            else
+                Console.WriteLine("C2 > C1");
+
+            #endregion
         }
     }
 }
