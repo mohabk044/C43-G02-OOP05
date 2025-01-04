bb95d4fc335c35f47f92876493e8047fef59d0c8
Mohab Khaled <mohabk044@gmail.com>
2025-01-04 22:07:45 +0200

    First Project

---
+++ Assignment/First_Project/Maths.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.First_Project
+{
+    internal static class Maths
+    {
+        #region Test
+        //public Maths(int num1, int num2)
+        //{
+        //    Num1 = num1;
+        //    Num2 = num2;
+        //}
+
+        //public int Num1 { get; set; }
+        //public int Num2 { get; set; }
+
+        //public int Sum()
+        //{
+        //    return Num1 + Num2;
+        //}
+        //public int Subtract()
+        //{
+        //    return Num1 - Num2;
+        //}
+        //public int Multiply()
+        //{
+        //    return Num1 * Num2;
+        //}
+        //public int Divided()
+        //{
+        //    return Num1 / Num2;
+        //} 
+        #endregion
+
+        public static int Sum(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+        public static int Subtract(int Num1, int Num2)
+        {
+            return Num1 - Num2;
+        }
+        public static int Multiply(int Num1, int Num2)
+        {
+            return Num1 * Num2;
+        }
+        public static double Divided(double Num1, double Num2)
+        {
+            if (Num2 != 0)
+                return Num1 / Num2;
+            Console.Write("Error! Cannot Divided By Zero ");
+            return 0;
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using Assignment.First_Project;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region First Project
+            #region Test
+            //Maths maths1 = new Maths(4,4);
+            //int Result1 = maths1.Sum();
+            //Maths maths2 = new Maths(4,4);
+            //int Result2 = maths2.Subtract();
+            //Maths maths3 = new Maths(4,4);
+            //int Result3 = maths3.Multiply();
+            //Maths maths4 = new Maths(4,4);
+            //int Result4 = maths4.Divided();
+            //Console.WriteLine($"Sum: {Result1}");
+            //Console.WriteLine($"Subtract: {Result2}");
+            //Console.WriteLine($"Multiply: {Result3}");
+            //Console.WriteLine($"Divided: {Result4}"); 
+            #endregion
+
+            // Use Static Keyword
+            //Maths maths = new Maths();  // Invalid
+            //Maths maths;  // Invalid
+            //Console.WriteLine($"Addition: {Maths.Sum(3, 4)}");
+            //Console.WriteLine($"Subtract: {Maths.Subtract(6, 2)}");
+            //Console.WriteLine($"Multiply: {Maths.Multiply(5, 8)}");
+            //Console.WriteLine($"Divided: {Maths.Divided(8, 2)}");
+            //Console.WriteLine($"Divided: {Maths.Divided(8, 33)}");
+            //Console.WriteLine(Maths.Divided(8, 0)); 
+            #endregion
+        }
+    }
+}
