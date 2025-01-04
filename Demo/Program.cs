708b1fc7dfdf5ed1bb1cc1eda51e6c882ef16d8e
Mohab Khaled <mohabk044@gmail.com>
2025-01-04 21:52:24 +0200

    Static

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -72,6 +72,16 @@ namespace Demo
             //Console.WriteLine(circle.Premiter);
             #endregion
 
+            #region Static
+            //Utility U1 = new Utility() { X = 10, Y = 20 };
+            //Utility U2 = new Utility() { X = 30, Y = 40 };
+            //Console.WriteLine(Utility.CmInch(20));   // Class Member Method {Static}
+            //Console.WriteLine(Utility.CmInch(20));  // Class Member Method {Static}
+            //Console.WriteLine(U1.AreaCircle(10));   // Object Member Method {Non Static}
+            //Console.WriteLine(Utility.AreaCircle(10));
+            #endregion
+
+
         }
     }
 }
---
+++ Demo/Static/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Static
+{
+    internal static class Utility
+    {
+        public static double CmInch(double cm)
+        {
+            return cm / 2.54;
+        }
+
+        //private static double pi = 3.14;
+        private const double pi = 3.14;
+
+        public static double Pi
+        {
+            get { return pi; }
+        }
+
+        public static double AreaCircle(double redius)
+        {
+            return redius * redius * Pi;
+        }
+    }
+}
